434b0112132204ee5bac6418b0841371796d44e6
autodocs <autodocs>
2018-09-02 07:32:59 +0000

    build based on 9bb461f

---
+++ latest/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Psycho.jl Documentation",
+    "title": "Psycho.jl Documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Psycho.jl-Documentation-1",
+    "page": "Psycho.jl Documentation",
+    "title": "Psycho.jl Documentation",
+    "category": "section",
+    "text": "CurrentModule = Psychof(x)link to Psycho.jl Documentation\nlink to f(x)"
+},
+
+]}

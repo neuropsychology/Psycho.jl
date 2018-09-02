570a1b77c153669e0e568d6c7b1156e86b98eada
DominiqueMakowski <dom.mak19@gmail.com>
2018-09-02 16:42:43 +0200

    export groupnames

--- src/Psycho.jl
+++ src/Psycho.jl
@@ -2,10 +2,12 @@ module Psycho
 
 export
     standardize,
-    standardize!
+    standardize!,
+    simulate_groupnames
 
 
 include("core/standardize.jl")
+include("simulate/groupnames.jl")
 # include("simulate/correlation.jl")
 
 end # module

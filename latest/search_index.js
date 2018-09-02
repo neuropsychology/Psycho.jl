dc15c36f959c24daba0dc4238f7399be8a60556e
autodocs <autodocs>
2018-09-02 09:34:14 +0000

    build based on 486b149

--- latest/search_index.js
+++ latest/search_index.js
@@ -2,18 +2,50 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "index.html#",
-    "page": "Psycho.jl Documentation",
-    "title": "Psycho.jl Documentation",
+    "page": "Psycho.jl",
+    "title": "Psycho.jl",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "index.html#Psycho.jl-Documentation-1",
-    "page": "Psycho.jl Documentation",
-    "title": "Psycho.jl Documentation",
+    "location": "index.html#Psycho.jl-1",
+    "page": "Psycho.jl",
+    "title": "Psycho.jl",
     "category": "section",
     "text": "Welcome to Psycho\'s for Julia documentation."
+},
+
+{
+    "location": "index.html#Status-and-News-1",
+    "page": "Psycho.jl",
+    "title": "Status and News",
+    "category": "section",
+    "text": "2018/09/02: Initial release of the documentation."
+},
+
+{
+    "location": "index.html#Content-1",
+    "page": "Psycho.jl",
+    "title": "Content",
+    "category": "section",
+    "text": "Pages = [\n    \"man/API.md\"\n]\nDepth = 1"
+},
+
+{
+    "location": "man/API.html#",
+    "page": "API",
+    "title": "API",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "man/API.html#API-1",
+    "page": "API",
+    "title": "API",
+    "category": "section",
+    "text": "Details about Psycho\'s functions."
 },
 
 ]}

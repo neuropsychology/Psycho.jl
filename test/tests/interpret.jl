ad3ea2e696e1b8c54b89bbb0eb9577eb1513ff88
DominiqueMakowski <dom.mak19@gmail.com>
2018-09-03 17:36:03 +0200

    fix tests

--- test/tests/interpret.jl
+++ test/tests/interpret.jl
@@ -3,6 +3,6 @@ p_rules = Rules([0.05], ["significant", "not significant"], true)
 @test interpret(0.04, p_rules) == "significant"
 
 
-@test interpret_p(0.04) == "significant"
-@test format_p(0.005, stars=true) == "p < .01*"
-@test format_p(0.0001, stars=true) == "p < .001*"
+@test format_p(0.04, stars=true) == "p < .05*"
+@test format_p(0.005, stars=true) == "p < .01**"
+@test format_p(0.0001, stars=true) == "p < .001***"

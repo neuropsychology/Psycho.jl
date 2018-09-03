700b17a3ea8a1d91fa95f6eefb73934b56ab26cd
DominiqueMakowski <dom.mak19@gmail.com>
2018-09-03 08:27:59 +0200

    add tests

--- test/tests/simulate.jl
+++ test/tests/simulate.jl
@@ -1,8 +1,9 @@
+import Statistics
 
 @test length(simulate_groupnames(10)) == 10
 @test length(simulate_coefs_correlation(10)) == 10
 
 data = simulate_data_correlation(0.2)
-@test cor(data[:y], data[:Var1]) ≈ 0.2
+@test Statistics.cor(data[:y], data[:Var1]) ≈ 0.2
 
 @test length(simulate_data_correlation([[0.2], [0.1]])) == 3
